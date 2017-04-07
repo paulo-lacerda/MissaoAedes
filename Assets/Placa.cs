35be6dc987222a9b6a114dbef35b9a8a1e12a126
PV <vinicius.skp@gmail.com>
2017-04-07 16:51:52 -0300

    placa falando objetivo da fase mostrando mensagem

---
+++ Assets/Placa.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Placa : MonoBehaviour {
+
+	// Use this for initialization
+
+	public SpriteRenderer sprender;
+
+	void Start () {
+
+		sprender.enabled = false;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		esconder ();
+	
+	}
+
+
+	void esconder(){
+
+		sprender = gameObject.GetComponent<SpriteRenderer> ();
+
+
+	}
+
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Player") {
+			sprender.enabled = true;
+
+			Destroy(gameObject, 6f);
+
+			
+		
+		} 
+	}
+
+
+
+}
