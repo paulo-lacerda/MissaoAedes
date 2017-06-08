2ef36445eb106be0e2049b53eb5d8b04ecd775a2
PV <vinicius.skp@gmail.com>
2017-06-07 23:36:21 -0300

    plataformas dinamicas

---
+++ Assets/Scripts/PlataformaVoadora.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaVoadora : MonoBehaviour {
+
+	// Use this for initialization
+
+	public bool esquerda;
+	public float velocidade;
+	public float delay;
+
+	public float tempoPlataforma; 
+	public float delayPlataforma;
+
+	public float tempoMovimento;
+
+
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Movimentar ();
+
+	}
+
+
+	void Movimentar(){
+
+		tempoMovimento += Time.deltaTime;
+
+		if (tempoMovimento <= delay) {
+		
+		
+			if (esquerda) {
+			
+				transform.Translate (-Vector2.right * velocidade * Time.deltaTime);
+			} 
+
+			else {
+
+				transform.Translate ( -Vector2.left * velocidade * Time.deltaTime);
+			}
+
+		}
+		
+					else{
+
+						esquerda = !esquerda;
+						tempoMovimento = 0;
+					}
+
+	}
+}
